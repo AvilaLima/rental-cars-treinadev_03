ee75ab931aa27ab607aa9fb97ca1ed76a7193c64
Alexandre Lima <lima@fatecsp.br>
2020-04-19 00:05:57 -0300

    [Admin] Cadastrar categorias com preços padrão

--- app/controllers/carcategories_controller.rb
+++ app/controllers/carcategories_controller.rb
@@ -6,6 +6,18 @@ class CarcategoriesController < ApplicationController
           render :index
         end
     end
+    def new
+        @carcategory = Carcategory.new
+    end
+    def create
+        @carcategory = Carcategory.new
+        @carcategory.name = params[:carcategory][:name]
+        @carcategory.daily_rate = params[:carcategory][:daily_rate]
+        @carcategory.car_insurance = params[:carcategory][:car_insurance]
+        @carcategory.third_party_insurance = params[:carcategory][:third_party_insurance]
+        @carcategory.save
+        redirect_to @carcategory        
+    end
     def show
         id= params[:id]
         @carcategory = Carcategory.find(id)
---
+++ spec/admin_register_carcategories_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Admin register manufacturer' do
+  scenario 'from index page' do
+    visit root_path
+    click_on 'Categorias'
+
+    expect(page).to have_link('Registrar nova categoria', href: new_carcategory_path)
+  end
+
+  scenario 'successfully' do
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Registrar nova categoria'
+
+    fill_in 'Nome', with: 'Van'
+    fill_in 'Diária', with: '400'
+    fill_in 'Seguro do carro', with: '500'
+    fill_in 'Seguro contra terceiros', with: '1000'
+    click_on 'Enviar'
+
+    expect(current_path).to eq carcategory_path(Carcategory.last.id)
+    expect(page).to have_content('Van')
+    expect(page).to have_content('400')
+    expect(page).to have_content('500')
+    expect(page).to have_content('1000')
+    expect(page).to have_link('Voltar')
+  end
+end
