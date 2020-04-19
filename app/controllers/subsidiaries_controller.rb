bc5294fe7be958e20e73efbc6a32eb75a5db477c
Alexandre Lima <lima@fatecsp.br>
2020-04-18 23:50:15 -0300

    [Admin] Cadastrar uma filial

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -6,6 +6,17 @@ class SubsidiariesController < ApplicationController
         render :index
       end
     end
+    def new
+      @subsidiary = Subsidiary.new 
+    end
+    def create
+      @subsidiary = Subsidiary.new
+      @subsidiary.name = params[:subsidiary][:name]
+      @subsidiary.cnpj = params[:subsidiary][:cnpj]      
+      @subsidiary.address = params[:subsidiary][:address]
+      @subsidiary.save
+      redirect_to @subsidiary
+    end
     def show
       id = params[:id]
       @subsidiary = Subsidiary.find(id)
