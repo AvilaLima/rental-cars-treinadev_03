5615410439234d29e633cf1bbdd84a75faedf4e6
Alexandre Lima <lima@fatecsp.br>
2020-04-18 22:30:44 -0300

    [Admin] Cadastrar um fabricante

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -6,6 +6,15 @@ class ManufacturersController < ApplicationController
       render :index
     end
   end
+  def new
+    @manufacturer = Manufacturer.new
+  end
+  def create
+    @manufacturer = Manufacturer.new
+    @manufacturer.name = params[:manufacturer][:name]
+    @manufacturer.save
+    redirect_to @manufacturer
+  end
   def show
     id = params[:id]
     @manufacturer = Manufacturer.find(id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/', to: home#index   
   root to: 'home#index'
-  resources :manufacturers
-  resources :subsidiaries
-  resources :carcategories
+  resources :manufacturers, only: [:index, :show, :new, :create]
+  resources :subsidiaries, only: [:index, :show, :new, :create]
+  resources :carcategories, only: [:index, :show, :new, :create]
 end
