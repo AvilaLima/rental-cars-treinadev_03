59d7762813d260c5fc0cbf6bc711d8e547de958b
Alexandre Lima <lima@fatecsp.br>
2020-04-24 03:39:52 -0300

    Resolvendo possiveis ambiguidades nos testes e resolvendo o caso de não ter modelos

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -1,8 +1,12 @@
 class CarModelsController < ApplicationController
-    def index
-        @car_models = CarModel.all
-    end
-    def show
-        @car_model = CarModel.find(params[:id])
-    end
+	def index
+		@car_models = CarModel.all
+		if CarModel.count == 0
+			flash[:notice] = 'Nenhum modelo de carro cadastrado'
+			render :index
+		end
+	end
+	def show
+		@car_model = CarModel.find(params[:id])
+	end
 end
--- spec/features/carmodel/admin_view_car_model_spec.rb
+++ spec/features/carmodel/admin_view_car_model_spec.rb
@@ -26,7 +26,11 @@ feature 'Admin view car models' do
         expect(page).to have_content '2021'
     end
     scenario 'no car models' do
-        
+       
+        visit root_path
+        click_on 'Modelos de Carros'
+    
+        expect(page).to have_content('Nenhum modelo de carro cadastrado') 
     end
 
     scenario 'and view details' do
@@ -34,13 +38,20 @@ feature 'Admin view car models' do
         #arrange
         fiat =  Manufacturer.create!(name: 'Fiat')
         cat_a= Carcategory.create!(name: 'Coupe',daily_rate:500,car_insurance: 150,third_party_insurance:300)
-        CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat, motorization: '1.0', fuel_type: 'Flex', carcategory: cat_a)      
+        uno = CarModel.create!(name: 'Uno', year: 2020, manufacturer: fiat, motorization: '1.0', fuel_type: 'Flex', carcategory: cat_a)    
+        
+        cat_e= Carcategory.create!(name: 'Caminhonete',daily_rate:200,car_insurance: 350,third_party_insurance:500)
+        CarModel.create!(name: 'Toro', year: 2020, manufacturer: fiat, motorization: '2.0', fuel_type: 'Diesel', carcategory: cat_e)     
 
         #act
         visit root_path
         click_on 'Modelos de Carros'
-        click_on 'Ver Detalhes'
-
+        #click_on 'Ver Detalhes'
+        #click_on "details-#{uno.id}" ou find("a#details-#{uno.id}").click()
+        within("tr#car-model-#{uno.id}")do
+            #page = escopo do seletor CSS
+            click_on 'Ver Detalhes'
+        end
         #assert
         expect(page).to have_content 'Uno'
         expect(page).to have_content 'Fabricante: Fiat'
@@ -50,5 +61,9 @@ feature 'Admin view car models' do
         expect(page).to have_content 'Diária: R$ 500,00'
         expect(page).to have_content 'Seguro do Carro: R$ 150,00'
         expect(page).to have_content 'Seguro contra terceiros: R$ 300,00'
+
+
+        expect(page).not_to have_content 'Toro'
+        expect(page).not_to have_content 'Motor: 2.0'
     end
 end
